29a2b89b019d8734210bddb8e584b95cd059355d
drkwitht <derektan3510@gmail.com>
2025-05-04 13:15:54 -0700

    Updated vertex shader for tile renders.

--- shaders/vertex_shader.glsl
+++ shaders/vertex_shader.glsl
@@ -1,8 +1,29 @@
 #version 330 core
 
+/// NOTE each point position in model comprises a fully Q4 square!
 layout (location = 0) in vec3 aPos;
-uniform mat4 dTransform;
+
+// shrinks dims. to tile size
+uniform float shrinkWidthFactor;
+uniform float shrinkHeightFactor;
+
+// places tile at equivalent position to board offset
+uniform mat4 placeTransform;
 
 void main() {
-    gl_Position = dTransform * vec4(aPos, 1.0f);
+    const mat4 originTransform = mat4(
+        1.0f, 0.0f, 0.0f, -1.0f,
+        0.0f, 1.0f, 0.0f, 1.0f,
+        0.0f, 0.0f, 1.0f, 0.0f,
+        0.0f, 0.0f, 0.0f, 1.0f
+    );
+
+    const mat4 shrinkTransform = mat4(
+        1.0f / shrinkWidthFactor, 0.0f, 0.0f, 0.0f,
+        0.0f, 1.0f / shrinkHeightFactor, 0.0f, 0.0f,
+        0.0f, 0.0f, 1.0f, 0.0f,
+        0.0f, 0.0f, 0.0f, 1.0f
+    );
+
+    gl_Position = placeTransform * shrinkTransform * originTransform * vec4(aPos, 1.0f);
 }
